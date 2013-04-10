34d0a66afeb9d52c59ffadeb148fbde888d172ef
Manuel Jesus Fernandez <manueljesusfernandez@icloud.com>
2013-04-10 11:39:48 +0200

    cambios optimizacion

--- public_html/js/ejercicios/1_bloquesmultibase.js
+++ public_html/js/ejercicios/1_bloquesmultibase.js
@@ -14,7 +14,29 @@ var capaBotones = new Kinetic.Layer();
 var capaCubos = new Kinetic.Layer();
 
 var offsetX = 0;
-var cubos = {};
+var imgCubo = new Image();
+
+function dibujarCubo(img) {
+	var cubo = new Kinetic.Image({
+		x : offsetX,
+		y : 50,
+		image : img,
+		width : 54,
+		height : 55,
+		draggable : true
+	});
+	
+	cubo.on('mouseover', function() {
+		document.body.style.cursor = 'pointer';
+	});
+	cubo.on('mouseout', function() {
+		document.body.style.cursor = 'default';
+	});
+	
+	capaCubos.add(cubo);
+	offsetX += 30;
+}
+
 
 $(document).ready(function() {
 	container = $('#container');
@@ -43,40 +65,32 @@ $(document).ready(function() {
 	botonUnidades.on('click', function() {
 		var img = new Image();
 		img.onload = function() {
-			var cubo = new Kinetic.Image({
-				x : offsetX,
-				y : 50,
-				image : img,
-				width : 54,
-				height : 55,
-				draggable : true
-			});
-
-			capaCubos.add(cubo);
-
+			dibujarCubo(img);
 		};
-		//
-		capaCubos.draw();
+		
 		img.src = "/didactica-matematicas/public_html/img/cubo.png";
-		offsetX += 30;
+		
+		capaCubos.draw();
+		
+		
 	});
-	
+
 	var botonLimpiar = new Kinetic.Rect({
-		x: 60,
-		y: 0,
-		width: 100,
-		height: 50,
-		fill: 'blue',
-		stroke: 'black',
-		strokeWidth: 4
+		x : 60,
+		y : 0,
+		width : 100,
+		height : 50,
+		fill : 'blue',
+		stroke : 'black',
+		strokeWidth : 4
 	});
 	botonLimpiar.on('click', function() {
-		capaCubos.remove();
+		capaCubos.removeChildren();
 		capaCubos = new Kinetic.Layer();
 		escenario.add(capaCubos);
 		offsetX = 0;
 	});
-	
+
 	capaBotones.add(botonUnidades);
 	capaBotones.add(botonLimpiar);
 	capaBotones.draw();
