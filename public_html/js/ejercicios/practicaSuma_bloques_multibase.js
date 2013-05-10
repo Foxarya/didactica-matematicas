1d5dfba1d092b7b08ac91d5d301d833aa2431d29
Carlos Ocaña <carlosocana@MacBook-Pro-de-Carlos-Ocana.local>
2013-05-10 09:40:43 +0200

    cuadros de texto

---
+++ public_html/js/ejercicios/practicaSuma_bloques_multibase.js
@@ -0,0 +1,4 @@
+
+$("<input type='text' style='position:relative; left:50px; width: 80px' readonly ></input>").prependTo('.main');
+
+$("<input type='text' style='position:relative; float:right; right:50px; width: 80px; ' ></input>").prependTo('.main');
